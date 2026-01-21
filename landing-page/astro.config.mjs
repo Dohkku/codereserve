6e28dace52dae291bdd461ee438c4e00bef5813f
Dohkku <47825093+Dohkku@users.noreply.github.com>
2026-01-21 16:18:01 +0100

    netlify

--- landing-page/astro.config.mjs
+++ landing-page/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es']
-  }
+  },
+
+  adapter: netlify()
 });
