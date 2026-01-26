0837ed8cf23080cc27ee3f2a819d22db7a876eb8
dohkku <frederickandradeperez@gmail.com>
2026-01-26 19:41:34 +0100

    new copy

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -2,13 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
   output: 'static',
   adapter: vercel()
 });
